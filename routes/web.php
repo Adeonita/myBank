b7ff3e246832c767e89a44b1f58d456c08387a2d
adeonita <adeonita.sousa@gmail.com>
2021-08-01 02:21:21 -0300

    chore(user): add routes create and get

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/user/create', 'UserController@create');
+$router->get('/user/{document}', 'UserController@find');
