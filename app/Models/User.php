cdb97a54bb2c7702200b2b3f4315464a7ecd7025
adeonita <adeonita.sousa@gmail.com>
2021-08-01 02:18:53 -0300

    feat(user): add user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,25 +9,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model
 {
-    use Authenticatable, Authorizable, HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    public $timestamps = false;
+
     protected $fillable = [
-        'name', 'email',
+        'firstName', 'lastName', 'document', 'email', 'password', 'phoneNumber', 'type'
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password',
     ];
+
+    public function wallet(){
+        return $this->hasOne(Wallet::class);
+    }
 }
