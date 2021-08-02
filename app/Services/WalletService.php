c24e9b352401a038c351aee63527d2d23977a28f
adeonita <adeonita.sousa@gmail.com>
2021-08-02 10:27:03 -0300

    feat(wallet): add getBalance method

--- app/Services/WalletService.php
+++ app/Services/WalletService.php
@@ -5,7 +5,12 @@
     use App\Http\Interfaces\WalletServiceInterface;
 
     class WalletService implements WalletServiceInterface {
-        
+
+        public function getBalance($userId){
+            $wallet = Wallet::firstWhere('user_id', $userId);
+            return $wallet->money;
+        }
+
         public static function create($userId): void {
             Wallet::create([
                 'user_id' => $userId,
