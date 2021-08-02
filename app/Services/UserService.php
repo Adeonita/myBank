fbd99de842534753576031962576f91703f5b210
adeonita <adeonita.sousa@gmail.com>
2021-08-02 10:26:02 -0300

    feat(user): add getById method and transaction to create user

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,26 +1,31 @@
 <?php
     namespace App\Services;
 
-    use Exception;
     use App\Models\User;
     use App\Services\WalletService;
-    use App\Http\Interfaces\UserServiceInterface;
     use App\Exceptions\UserNotFound;
+    use Illuminate\Support\Facades\DB;
+    use App\Http\Interfaces\UserServiceInterface;
 
-    class UserService implements UserServiceInterface  {
+class UserService implements UserServiceInterface  {
         
         public static function create($user): void {
             try {              
-               $user =  User::create($user);
-               WalletService::create($user->id);
-            } catch(Exception $e) {
-                response()->json([
-                    'message' => $e
+                DB::beginTransaction();
+                    $user =  User::create($user);
+                    WalletService::create($user->id);
+                DB::commit();
+
+            } catch(\Exception $e) {
+                DB::rollBack();
+
+                echo response()->json([
+                    'message' => $e->getMessage()
                 ], 400);
             }
         }
 
-        public static function get(string $document): User {
+        public static function getByDocument(string $document): User {
             $user = User::firstWhere('document',$document);
             
             if (!$user){
@@ -28,5 +33,10 @@
             }
             
             return $user;
+        }
+
+
+        public static function getById(string $id): User {
+            return User::find($id);
         }
     }
