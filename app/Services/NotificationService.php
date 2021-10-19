7ed1da03317ef1b67c2539c125de28c6424acc4b
adeonita <adeonita.sousa@picpay.com>
2021-10-19 14:31:26 -0300

    refactor: remove construct method

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -3,16 +3,9 @@ namespace App\Services;
 
 use App\Http\Interfaces\NotificationInterface;
 use App\Jobs\NotificationJob;
-use App\Services\AuthorizationNotificationService;
 
 class NotificationService implements NotificationInterface
 {
-    private $authorizationNotificationService;
-
-    public function __construct(AuthorizationNotificationService $authorizationNotificationService)
-    {
-        $this->authorizationNotificationService = $authorizationNotificationService;
-    }
     public function send($email, $phoneNumber, $value, $payerName)
     {
         dispatch(new NotificationJob($email, $phoneNumber, $value, $payerName));
