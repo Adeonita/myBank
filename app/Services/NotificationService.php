ef428039cc0938d564fa553ecdac6f7f31258f1f
adeonita <adeonita.sousa@picpay.com>
2021-10-29 19:31:49 -0300

    feat(notification): add call to email job

--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -2,14 +2,14 @@
 namespace App\Services;
 
 use App\Http\Interfaces\NotificationInterface;
-use App\Jobs\NotificationJob;
+use App\Jobs\EmailJob;
 use App\Jobs\SmsJob;
 
 class NotificationService implements NotificationInterface
 {
     public function send($payeeEmail, $payeePhoneNumber, $value, $payerName)
     {
-        dispatch(new NotificationJob($payeeEmail, $payeePhoneNumber, $value, $payerName));
+        dispatch(new EmailJob($payeeEmail, $value, $payerName))->onQueue('email');
         dispatch(new SmsJob($payeePhoneNumber, $value, $payerName))->onQueue('sms');
     }
 }
