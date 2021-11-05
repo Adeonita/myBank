19d8fc0bd9af1e4bff7ac2a969b6466ac4a77a79
adeonita <adeonita.sousa@picpay.com>
2021-11-05 00:24:37 -0300

    feat(wallet): register wallet service provider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -93,6 +93,7 @@ $app->configure('app');
 
 $app->register(Enqueue\LaravelQueue\EnqueueServiceProvider::class);
 $app->register(App\Providers\UserServiceProvider::class);
+$app->register(App\Providers\WalletServiceProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
